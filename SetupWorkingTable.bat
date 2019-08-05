469090e0a6fda2eb258441d515537781f149d5ea
ALYMBEK <33069225+ALYMBEK-KG@users.noreply.github.com>
2019-08-05 16:55:08 +0600

    Update SetupWorkingTable.bat

--- SetupWorkingTable.bat
+++ SetupWorkingTable.bat
@@ -4,7 +4,6 @@ set DIR=%SystemRoot%\System32\GroupPolicy\User\Scripts
 set FILE=%DIR%\Logon\logon.bat
 echo set DIR=%%UserProfile%%\Documents\WorkingTable> %FILE%
 echo md %%DIR%%>> %FILE%
-echo set FILE=%%DIR%%\WorkingTable.txt>> %FILE%
 echo set CUR_YYYY=%%date:~6,4%%>> %FILE%
 echo set CUR_MM=%%date:~3,2%%>> %FILE%
 echo set CUR_DD=%%date:~0,2%%>> %FILE%
@@ -12,12 +11,12 @@ echo set CUR_HH=%%time:~0,2%%>> %FILE%
 echo set CUR_II=%%time:~3,2%%>> %FILE%
 echo set CUR_SS=%%time:~6,2%%>> %FILE%
 echo set DATA=%%CUR_YYYY%%/%%CUR_MM%%/%%CUR_DD%%, %%CUR_HH%%:%%CUR_II%%:%%CUR_SS%%>> %FILE%
-echo echo Logged in: "%%DATA%%"^>^> %%FILE%%>> %FILE%
+echo set FILE=%%DIR%%\WorkingTable_%%CUR_YYYY%%-%%CUR_MM%%.txt>> %FILE%
+echo echo Coming: "%%DATA%%"^>^> %%FILE%%>> %FILE%
 
 set FILE=%DIR%\Logoff\logoff.bat
 echo set DIR=%%UserProfile%%\Documents\WorkingTable> %FILE%
 echo md %%DIR%%>> %FILE%
-echo set FILE=%%DIR%%\WorkingTable.txt>> %FILE%
 echo set CUR_YYYY=%%date:~6,4%%>> %FILE%
 echo set CUR_MM=%%date:~3,2%%>> %FILE%
 echo set CUR_DD=%%date:~0,2%%>> %FILE%
@@ -25,5 +24,6 @@ echo set CUR_HH=%%time:~0,2%%>> %FILE%
 echo set CUR_II=%%time:~3,2%%>> %FILE%
 echo set CUR_SS=%%time:~6,2%%>> %FILE%
 echo set DATA=%%CUR_YYYY%%/%%CUR_MM%%/%%CUR_DD%%, %%CUR_HH%%:%%CUR_II%%:%%CUR_SS%%>> %FILE%
-echo echo Logged out: "%%DATA%%"^>^> %%FILE%%>> %FILE%
-echo echo ----------------------------^>^> %%FILE%%>> %FILE%
+echo set FILE=%%DIR%%\WorkingTable_%%CUR_YYYY%%-%%CUR_MM%%.txt>> %FILE%
+echo echo Leaving: "%%DATA%%"^>^> %%FILE%%>> %FILE%
+echo echo ------------------------------^>^> %%FILE%%>> %FILE%
